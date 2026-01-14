d91fdc22dd82f6ed1708c75c75dbaeed0021a0f3
JasonFoster0809 <donguyenduykhang012340@gmail.com>
2026-01-14 14:07:19 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { 
-  LogIn, PlusCircle, Home, ShoppingBag, 
+  LogIn, PlusCircle, Home, ShoppingBag, Share2,
   User, LogOut, ShieldCheck, Heart, Package 
 } from 'lucide-react';
 import { useAuth } from '../contexts/AuthContext';
